f7a59fe53c9f0ae616f60e26d980e39a56e07646
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-06-01 10:37:32 +0800

    chore: bump vendorHash nix

--- nix/package.nix
+++ nix/package.nix
@@ -123,7 +123,7 @@ else
     # `nix-shell -p go --run 'go mod vendor'`
     # `nix hash path vendor`
     # `rm -rf vendor`
-    vendorHash = "sha256-70+Ee4VSCp7IaKNPNIHtYCK/XGgLKbdPh/5zRp/qS9M=";
+    vendorHash = "sha256-Wr+ZDdRIzw3U2+ThoJy96SO10f8MKKhAOoWFXKlVdRg=";
 
     ldflags = [
       "-s"
