b361a5cc88e9fcf3429768aaffbfdabdd49a5a8e
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-06-01 11:03:21 +0800

    chore: bump flake to v0.1.0

--- nix/package.nix
+++ nix/package.nix
@@ -23,12 +23,12 @@ if useZip then
         "aarch64-darwin" = {
           url = "https://github.com/y3owk1n/mimi/releases/download/v${version}/mimi-darwin-arm64.zip";
           # run `nix hash convert --hash-algo sha256 (nix-prefetch-url https://github.com/y3owk1n/mimi/releases/download/v0.1.0/mimi-darwin-arm64.zip)`
-          sha256 = "";
+          sha256 = "sha256-cKvefd8wylD4R46knd0QNq4WY4OlbcPsCPNaRjHtSJk=";
         };
         "x86_64-darwin" = {
           url = "https://github.com/y3owk1n/mimi/releases/download/v${version}/mimi-darwin-amd64.zip";
           # run `nix hash convert --hash-algo sha256 (nix-prefetch-url https://github.com/y3owk1n/mimi/releases/download/v0.1.0/mimi-darwin-amd64.zip)`
-          sha256 = "";
+          sha256 = "sha256-q+Ed5vAxSdfilJfFb1yisVkKX1PQIozLogKlp+MPWis=";
         };
       }
       .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
