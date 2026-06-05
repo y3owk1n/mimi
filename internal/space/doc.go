43504fdbf1be35a35394af0c9dab7a39cb0ee2f8
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-06-05 17:37:31 +0800

    Revert "feat: add `space`, `focus_window` and `move_window_to_space` action (#24)"
    This reverts commit 0ad104438c010833a3ac399d676865cbf755f838.

--- internal/space/doc.go
+++
@@ -1,6 +0,0 @@
-// Package space exposes macOS Mission Control space and window
-// enumeration helpers used by the action package.
-//
-// All exported functions are macOS-only; mimi has no other
-// build targets.
-package space
