75b84ab042a0219cb28ee7153c31279fa40e17e6
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-09-13 12:57:20 +0800

    chore: bump flake to 0.16.0

--- nix/package.nix
+++ nix/package.nix
@@ -23,13 +23,13 @@ if useZip then
       {
         "aarch64-darwin" = {
           url = "https://github.com/y3owk1n/mimi/releases/download/v${version}/mimi-darwin-arm64.zip";
-          # run `nix hash convert --hash-algo sha256 (nix-prefetch-url https://github.com/y3owk1n/mimi/releases/download/v0.15.0/mimi-darwin-arm64.zip)`
-          sha256 = "sha256-0dHOotVhuIiPsfultRzLL/PdVC7vDaP9mf3EWv453h0=";
+          # run `nix hash convert --hash-algo sha256 (nix-prefetch-url https://github.com/y3owk1n/mimi/releases/download/v0.16.0/mimi-darwin-arm64.zip)`
+          sha256 = "sha256-Y4PV9t3iIxmnmkBZBdoKk+zL4LBtSqsFNMXyE3EjtTQ=";
         };
         "x86_64-darwin" = {
           url = "https://github.com/y3owk1n/mimi/releases/download/v${version}/mimi-darwin-amd64.zip";
-          # run `nix hash convert --hash-algo sha256 (nix-prefetch-url https://github.com/y3owk1n/mimi/releases/download/v0.15.0/mimi-darwin-amd64.zip)`
-          sha256 = "sha256-BEjPwl+9jYx/RjObxfSLOQwRETF0c9Fn33mzRWBpnzo=";
+          # run `nix hash convert --hash-algo sha256 (nix-prefetch-url https://github.com/y3owk1n/mimi/releases/download/v0.16.0/mimi-darwin-amd64.zip)`
+          sha256 = "sha256-nCpiXJI0SoW0Y2pGyhj7VplafHNkbakSDU9yDxbTIUw=";
         };
       }
       .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
